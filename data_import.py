ca33ac644bf6bb7a0ddb50942aa281791a8aae0c
nicolofp <nicolo.fp@gmail.com>
2024-07-12 03:07:35 -0400

    add details

--- data_import.py
+++ data_import.py
@@ -2,6 +2,7 @@ import pandas as pd
 import scipy as sp
 import numpy as np
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 dt = pd.read_csv("C:/Users/nicol/Documents/wind_production/Location1.csv")
 dt.drop("Time", axis=1).corr().round(2)
@@ -15,8 +16,14 @@ dt["thour"] = dt["Time"].dt.hour
 dt["tday"] = dt["Time"].dt.day 
 
 # Cyclic encoder
+# ToDo 
+dt.columns
 
+plt.scatter(dt["windspeed_100m"], dt["Power"])
+plt.show()
 
+plt.plot(dt.loc[dt["Time"] > datetime(2020,12,31,23,0,0),"Time"], dt.loc[dt["Time"] > datetime(2020,12,31,23,0,0),"Power"]) 
+plt.show()
 
 
 
